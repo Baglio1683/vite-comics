7a24ebe3e74e1ad22247cf2ed2bc50291938a848
MyUsername <alberto.baglivi@gmail.com>
2022-11-08 14:47:33 +0100

    Start Project

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+           message : "ciao"
+        }
+    }
+}
+
+</script>
+
+
+
+<template>
+   <h1>{{message}}</h1>
+</template>
+
+<style scope>
+
+   h1{
+    color : red
+   }
+
+</style>
