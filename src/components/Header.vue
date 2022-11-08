5ebc23c2ee29e5169606a2f9ba2313303bf79afa
MyUsername <alberto.baglivi@gmail.com>
2022-11-08 15:34:49 +0100

    Add the Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,67 @@ export default {
     name: "AppHeader",
     data() {
         return {
-           message : "ciao"
+            links: [
+                {
+                    title: "CHARACTERS",
+                    href: "/home",
+                    active: true,
+                },
+
+                {
+                    title: "COMICS",
+                    href: "/products",
+                    active: true,
+                },
+
+                {
+                    title: "MOVIES",
+                    href: "/about",
+                    active: true,
+                },
+
+                {
+                    title: "TV",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "GAMES",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "COLLECTIBLES",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "VIDEOS",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "FANS",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "NEWS",
+                    href: "/contacts",
+                    active: true,
+                },
+
+                {
+                    title: "SHOP",
+                    href: "/contacts",
+                    active: true,
+                },
+            ]
         }
     }
 }
@@ -14,13 +74,47 @@ export default {
 
 
 <template>
-   <h1>{{message}}</h1>
+
+   <header>
+
+    <div class="container">
+
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="(item) in links">{{item.title}}</li>
+            </ul>
+        </div>
+
+    </div>
+</header>
 </template>
 
-<style scope>
 
-   h1{
-    color : red
-   }
+
+<style lang="scss" scope>
+
+  header{
+    height: 20vh;
+    background-color: lightblue;
+
+    ul{
+        list-style: none;
+
+        li{
+            display: inline-block;
+            margin-right: 10px;
+
+            &:hover{
+                color: blue;
+            }
+        }
+    }
+  }
+
+
 
 </style>
